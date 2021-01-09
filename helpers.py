69ba800bee753cd296a5d4bdd3baa33846a051e8
Roj <rojserbest@gmail.com>
2021-01-09 16:54:55 +0300

    Fix showing null

--- helpers.py
+++ helpers.py
@@ -7,7 +7,7 @@ bot.sort_replies()
 
 def reply(text: str) -> str:
     res = bot.reply("localuser", text)
-    return res if res != "[ERR: No Reply Matched]" else None
+    return res if res != "[ERR: No Reply Matched]" else ""
 
 
 if __name__ == "__main__":
