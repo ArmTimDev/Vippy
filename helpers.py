2481c6c22f9e3bbd87513ecfd4193f98a35415f2
Roj <rojserbest@gmail.com>
2021-01-09 15:49:27 +0300

    Add test function to helpers.py

--- helpers.py
+++ helpers.py
@@ -4,5 +4,11 @@ bot = RiveScript(utf8=True)
 bot.load_directory("./scripts")
 bot.sort_replies()
 
+
 def reply(text: str) -> str:
     return bot.reply("localuser", text)
+
+
+if __name__ == "__main__":
+    while True:
+        print(reply(input()))
