f1b656ac7119334963d98231ddc76f5d543c4dee
Roj <49933115+rojserbest@users.noreply.github.com>
2021-01-09 15:45:28 +0300

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,8 @@
+from rivescript import RiveScript
+
+bot = RiveScript(utf8=True)
+bot.load_directory("./scripts")
+bot.sort_replies()
+
+def reply(text: str) -> str:
+    return bot.reply("localuser", msg)
