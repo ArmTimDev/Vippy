94faa9673975e0a15a3a7f05c7b9e1dc90b2fa62
GokuLover <68587810+sakshameng@users.noreply.github.com>
2021-02-09 17:42:35 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,7 +3,7 @@ from telegram.ext import CommandHandler
 
 def start(update, context):
     update.effective_message.reply_text(
-        "✋ Hello !\n🧠 I'm An Ai !\n❤ Made With Love By @BotLoversOfficial Team !"
+        "✋ Hello !\n I'm Vippy Ai Bot\n 🧠 I am Also known as chatbot I !\n❤ Made With Love By @VippyNews Team !\n Head to @VippyDiscussion\n ⚠️ NOTE : My AI Will only work on Groups.
     )
 
 
