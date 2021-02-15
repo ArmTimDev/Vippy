3b7c70f8a893643ece04ac33dfba96dd1fc39fa4
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2021-02-15 11:13:20 +0330

    Update Vippy.py

--- Vippy.py
+++ Vippy.py
@@ -6,7 +6,7 @@ from telegram.ext import Updater, CommandHandler, Filters
 from handlers import all_handlers
 
 
-updater = Updater("TOKEN")
+updater = Updater("1588959643:AAEgQ_veyW68tH6ZttY7clYAsia5o203AM0")
 
 for handler in all_handlers:
     if len(handler) == 2:
