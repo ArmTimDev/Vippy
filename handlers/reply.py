d8f1c399d0774ffca6f490b3e41a299fe68d5ff4
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2021-02-16 12:43:09 +0330

    Update reply.py

--- handlers/reply.py
+++ handlers/reply.py
@@ -1,23 +1,14 @@
-from telegram.ext import MessageHandler, Filters
+from telegram.ext import MessageHandler, Filters, CallbackContext
 from helpers import reply as r
+from gtts import gTTS
 
 
-def reply(update, context):
-    try:
-        update.effective_message.reply_text(
-            r(
-                update.effective_message.text
-            )
-        )
-    except:
-        pass
-
-
-__handlers__ = [
-    [
-        MessageHandler(
-            Filters.text & Filters.chat_type.groups,
-            reply
-        )
-    ]
-]
+def tts(update: Update, context: CallbackContext) -> None:
+    tts = update.effective_message.reply_text
+    myobj = gTTS(text=tts, lang=language, slow=False)
+    file_id = str(update.message.from_user.id) + '.mp3'
+    myobj.save(file_id)
+    context.bot.sendAudio(chat_id=update.message.chat.id, audio=open(file_id, 'rb'), title='Answer')
+    os.remove(file_id)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("tts", tts))
