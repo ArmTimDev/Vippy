284ace6c0d157ebbedd7399f487c04bd9412aacb
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2021-02-19 18:18:38 +0330

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,7 +3,7 @@ from telegram.ext import CommandHandler, Updater
 
 def start(update, context):
     update.effective_message.reply_text(
-        "✋ Hello !\n\n⚀ I'm An Ai !\n❤ Made With Love By @VippyNews Team !\n⚠️ Note : My Ai Will Work Only In Groups !"
+        "✋ Hello !\n\n⚀ I'm An Ai !\n❤ Made With Love By @VippyNews Team !"
     )
 
 __handlers__ = [
