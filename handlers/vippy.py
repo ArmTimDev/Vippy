9131aeedfda349b44fb53ad0754a8c00bec991da
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2021-02-20 08:00:30 +0330

    Update vippy.py

--- handlers/vippy.py
+++ handlers/vippy.py
@@ -1,7 +1,7 @@
 import random
 from telegram.ext import CommandHandler
 
-msg = ["Hi ! I'm Always Here !", "Hello ? Anyone Here ?", "I'm Online !", "Hello ? Anyone There ?", "I'm Busy Now, Contact Me Later"]
+msg = ["Hi ! I'm Always Here !", "Hello ? Anyone Here ?", "I'm Online !", "Hello ? Anyone There ?", "Hello, My Friend !"]
 
 def sendmsg(update, context):
     update.effective_message.reply_text(
