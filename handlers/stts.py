1ff4df36457acaadfa7b7897b860ffa377875369
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2021-02-20 12:33:25 +0330

    Update stts.py

--- handlers/stts.py
+++ handlers/stts.py
@@ -6,9 +6,9 @@ import os
 language = 'en'
 def stts(update: Update, context: CallbackContext) -> None:
  tts = update.message.text.replace(update.message.text.split(' ')[0], '')
- myobj = gTTS(text=tts, lang=language, slow=True)
+ obj = gTTS(text=tts, lang=language, slow=True)
  file_id = str(update.message.from_user.id) + '.mp3'
- myobj.save(file_id)
+ obj.save(file_id)
  context.bot.sendAudio(chat_id=update.message.chat.id, audio=open(file_id, 'rb'), title='Vippy Slow TTS')
  os.remove(file_id)
 
