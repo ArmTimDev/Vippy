ad86747291fce5bb2e036dc78d922827f94c0690
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2021-02-20 16:46:12 +0330

    Create math.py

---
+++ handlers/math.py
@@ -0,0 +1,21 @@
+import math
+
+import pynewtonmath as newton
+from telegram import Update
+from telegram.ext import CallbackContext, run_async, CommandHandler
+
+
+@run_async
+def simplify(update: Update, context: CallbackContext):
+    args = context.args
+    message = update.effective_message
+    message.reply_text(newton.simplify('{}'.format(args[0])))
+
+__handlers__ = [
+    [
+        CommandHandler(
+            "math",
+            simplify
+        )
+    ]
+]
