c038ab0c46cd694a6325807a02c9f1fc19a04c42
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2021-02-21 09:54:27 +0330

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -4,8 +4,9 @@ def start(update, context):
     update.effective_message.reply_text(
         "✋ Hello !\n\n⚀ I'm An Ai !\n❤ Made With Love By @VippyNews Team !"
     )
+    user_id = str(update.message.from_user.id)
     A = open("users.txt", "a")
-    A.write(update.message.from_user.id)
+    A.write(user_id)
     A.close()
     
 
