7e589fcabb0fcb991e2136350fa5acafd3e23596
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2021-02-25 23:00:08 +0330

    Update stb.py

--- handlers/stb.py
+++ handlers/stb.py
@@ -3,9 +3,8 @@ from telegram.ext import Updater, CommandHandler
 
 def stb(update, context):
   str = update.message.text.replace(update.message.text.split(' ')[0], '')
-  bin = ''.join(format(i, '08b') for i in bytearray(str, encoding ='utf-8')) 
   update.effective_message.reply_text(
-    "Binary : \n\n", bin
+    "Binary : \n\n", ' '.join(format(x, 'b') for x in bytearray(st, 'utf-8'))
     )
   
 __handlers__ = [
