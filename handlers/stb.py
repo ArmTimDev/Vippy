61abcc56e29bc99a8c3ddd6046665523f0087a5b
ArmTimDev <76068426+ArmTimDev@users.noreply.github.com>
2021-02-25 21:04:47 +0330

    Update stb.py

--- handlers/stb.py
+++ handlers/stb.py
@@ -5,7 +5,7 @@ def stb(update, context):
   str = update.message.text.replace(update.message.text.split(' ')[0], '')
   bin = ''.join(format(i, '08b') for i in bytearray(str, encoding ='utf-8')) 
   update.effective_message.reply_text(
-    "Binary : \n\n", str(bin)
+    "Binary : \n\n", bin
     )
   
 __handlers__ = [
