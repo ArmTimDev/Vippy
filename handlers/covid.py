d8781239b5f38a9a158882ee945d1c55160dbe1a
Vasu-Vijay <69088080+Vasu-Vijay@users.noreply.github.com>
2021-03-15 16:25:34 +0530

    Update covid.py

--- handlers/covid.py
+++ handlers/covid.py
@@ -1,9 +1,9 @@
 from telegram.ext import CommandHandler, Updater
-from covid19_data import JHU
+#from covid19_data import JHU
 
 def covid(update, context):
   update.effective_message.reply_text(
-        "🦠 COVID-19 Stats 🦠\n\n🌐 The Number Of Worldwide COVID-19 Recovered : " + str(JHU.Total.recovered) + "\n🌐 The Number Of Worldwide COVID-19 Confirmed : " + str(JHU.Total.confirmed) + "\n🌐 The Number Of Worldwide COVID-19 Deaths : " + str(JHU.Total.deaths) + "\n\n😷 Wear A Mask\n👋 Wash Your Hands Often\n🤧 Cover Your Coughs And Sneezes"
+        "😷 Wear A Mask\n👋 Wash Your Hands Often\n🤧 Cover Your Coughs And Sneezes\n\nBut sorry I can not display stats due to the problems, I will continue showing them soon :D"
     )
     
     
